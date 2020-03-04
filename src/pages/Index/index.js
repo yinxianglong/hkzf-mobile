c4ad2112d9a6cd6a435c57d03507037d111ec6b1
尹相龙 <yanlipingdegoutuizi@gmail.com>
2020-03-04 23:07:14 +0800

    首页轮播图
    使用轮播图组件完成首页轮播图，
    引入axios
    异步请求轮播图数据功能

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,45 @@
 import React from 'react'
-
+import { Carousel } from 'antd-mobile';
+import axios from 'axios'
 export default class Index extends React.Component{
-    render(){
-        return <div>我是index</div>
-    }
+      state = {
+        // 轮播图状态数据
+        swipers:[]
+      }
+
+      // 获取轮播图数据的方法
+      async getSwipers(){
+          const res = await axios.get('http://localhost:8080/home/swiper')
+          console.log('轮播图数据：',res)
+          this.setState({
+            swipers:res.data.body
+          })
+      }
+      renderSwipers(){
+        return this.state.swipers.map(item => (
+            <a
+            key={item.id}
+            href="http://www.alipay.com"
+            style={{ display: 'inline-block', width: '100%', height: 212 }}
+            >
+            <img
+                src={`http://localhost:8080${item.imgSrc}`}
+                alt=""
+                style={{ width: '100%', verticalAlign: 'top' }}
+            />
+            </a>
+        ))
+      }
+      componentDidMount() {
+        this.getSwipers()
+      }
+      render() {
+        return (
+            <div className="index">
+                <Carousel autoplay infinite >
+                {this.renderSwipers()}
+                </Carousel>
+            </div>
+        );
+      }
 }
